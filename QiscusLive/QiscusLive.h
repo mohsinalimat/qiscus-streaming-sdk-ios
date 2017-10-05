5d08d42810b797ea68b7a7fc9df89f28e96c5879
asharijuang <asharijuang@me.com>
2017-10-05 15:40:04 +0700

    Initial Commit

---
+++ QiscusLive/QiscusLive.h
@@ -0,0 +1,19 @@
+//
+//  QiscusLive.h
+//  QiscusLive
+//
+//  Created by asharijuang on 5/10/17.
+//  Copyright © 2017 asharijuang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for QiscusLive.
+FOUNDATION_EXPORT double QiscusLiveVersionNumber;
+
+//! Project version string for QiscusLive.
+FOUNDATION_EXPORT const unsigned char QiscusLiveVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <QiscusLive/PublicHeader.h>
+
+
