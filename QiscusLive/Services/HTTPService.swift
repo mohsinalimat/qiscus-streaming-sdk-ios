86ab80636f4852ae344e907c48b800646e649875
Qiscus <qiscus@Qicuss-MacBook-Pro.local>
2017-11-10 17:08:55 +0700

    sembarang

--- QiscusLive/Services/HTTPService.swift
+++ QiscusLive/Services/HTTPService.swift
@@ -29,51 +29,35 @@ class HttpClientApi: NSObject{
     
     
     
-    func makeAPICall(url: String,params: Dictionary<String, Any>?, method: HttpMethod, success:@escaping ( Data? ,HTTPURLResponse?  , NSError? ) -> Void, failure: @escaping ( Data? ,HTTPURLResponse?  , NSError? )-> Void) {
-        
+    func makeAPICall(url: String,params: Dictionary<String, Any>?, method: HttpMethod, headers: [String : String]? = nil, success:@escaping ( Data? ,HTTPURLResponse?  , NSError? ) -> Void, failure: @escaping ( Data? ,HTTPURLResponse?  , NSError? )-> Void) {
         request = URLRequest(url: URL(string: url)!)
-        
-        logging.print("URL = \(url)")
-        
+        print("URL = \(url)")
         if let params = params {
-            
-            
             let  jsonData = try? JSONSerialization.data(withJSONObject: params, options: .prettyPrinted)
-            
             request?.setValue("application/json", forHTTPHeaderField: "Content-Type")
             request?.httpBody = jsonData//?.base64EncodedData()
-            
-            
-            //paramString.data(using: String.Encoding.utf8)
+            if headers != nil {
+                request?.allHTTPHeaderFields = headers
+            }
         }
         request?.httpMethod = method.rawValue
-        
-        
         let configuration = URLSessionConfiguration.default
-        
         configuration.timeoutIntervalForRequest = 30
         configuration.timeoutIntervalForResource = 30
-        
         session = URLSession(configuration: configuration)
-        //session?.configuration.timeoutIntervalForResource = 5
-        //session?.configuration.timeoutIntervalForRequest = 5
-        
         session?.dataTask(with: request! as URLRequest) { (data, response, error) -> Void in
             
-            if let data = data {
-                
-                if let response = response as? HTTPURLResponse, 200...299 ~= response.statusCode {
-                    success(data , response , error as? NSError)
-                } else {
-                    failure(data , response as? HTTPURLResponse, error as? NSError)
-                }
-            }else {
-                
+        if let data = data {
+            if let response = response as? HTTPURLResponse, 200...299 ~= response.statusCode {
+                success(data , response , error as? NSError)
+            } else {
                 failure(data , response as? HTTPURLResponse, error as? NSError)
-                
             }
-            }.resume()
-        
+        }else {
+            failure(data , response as? HTTPURLResponse, error as? NSError)
+            
+        }
+        }.resume()
     }
     
 }
--- QiscusLive/Services/StreamService.swift
+++ QiscusLive/Services/StreamService.swift
@@ -30,31 +30,51 @@ class StreamService: NSObject {
     
     class func createStreaming(title : String, tags : String, completion: @escaping (Stream?)->()) {
         
-        let headers : HTTPHeaders = [
-            "Authorization" : "Bearer \(APPID)"
-        ]
+        var params  = [String : Any]()
+        params["tags"]  = tags
+        params["title"] = title
         
-        let params = [
-            "iostags" : "example : hello",
-            "title" : "title",
-        ]
-        print("params \(params), \(headers)")
-        
-        let request = Alamofire.request(StreamEndpoint.request, method: .post, parameters: params, headers: headers).responseJSON { (response)->Void in
-            print(response)
-            if response.result.value != nil {
-                if (response.response?.statusCode)! >= 300 {
-                    completion(nil)
-                } else {
-                    let json    = JSON(response.result.value ?? "")
-                    let result  = json["data"]
-                    let data    = Stream(withJSON: result)
-                    completion(data)
-                }
+        var headers = [String : String]()
+        headers["Authorization"] = "Bearer \(APPID)"
+        HttpClientApi.instance().makeAPICall(url: StreamEndpoint.request, params: params, method: HttpMethod.POST, headers: headers, success: { (data, response, error) in
+            if error != nil {
+                print(error!.localizedDescription)
             } else {
-                completion(nil)
+                if let data = data {
+                    if let stringData = String(data: data, encoding: String.Encoding.utf8) {
+                        print(stringData) //JSONSerialization
+                    }
+                }
             }
+        }) { (data, response, error) in
+            //
         }
-        print("request \(request)")
+//
+//        let headers : HTTPHeaders = [
+//            "Authorization" : "Bearer \(APPID)"
+//        ]
+//
+//        let params = [
+//            "iostags" : "example : hello",
+//            "title" : "title",
+//        ]
+//        print("params \(params), \(headers)")
+//
+//        let request = Alamofire.request(StreamEndpoint.request, method: .post, parameters: params, headers: headers).responseJSON { (response)->Void in
+//            print(response)
+//            if response.result.value != nil {
+//                if (response.response?.statusCode)! >= 300 {
+//                    completion(nil)
+//                } else {
+//                    let json    = JSON(response.result.value ?? "")
+//                    let result  = json["data"]
+//                    let data    = Stream(withJSON: result)
+//                    completion(data)
+//                }
+//            } else {
+//                completion(nil)
+//            }
+//        }
+//        print("request \(request)")
     }
 }
